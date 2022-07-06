aba5f3ae79ffa16ca2c23f9e4af51d5f62aff519
unknown <dylanjlenz@gmail.com>
2022-07-06 17:35:35 -0500

    Create Main, Sub, & Debug Files.

---
+++ main.py
@@ -0,0 +1,13 @@
+# (5): Store my destinations, restaurants, mode of transportations, and entertainments on separate lists. 
+# (5): Destination to be randomly selected. 
+# (5): Restaurant to be randomly selected. 
+# (5): Transportation to be randomly selected. 
+# (5): Entertainment to be randomly selected. 
+
+# (15): Randomly re-select a destination, restaurant, transportation, or entertainment. 
+# (10): Confirm that my day trip is “complete” once I like all of the random selections. 
+# (10): Display my completed trip in the console. 
+
+# (5): All of my functions have a Single Responsibility. Each function should do one thing.
+# (5): Make at least three commits with descriptive messages. 
+
