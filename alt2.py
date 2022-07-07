8bce371ba5db5c89f05757f857960ce9a14f32f1
unknown <dylanjlenz@gmail.com>
2022-07-07 12:05:08 -0500

    Create Core Functions.

--- alt2.py
+++ alt2.py
@@ -2,14 +2,59 @@
 # (10): Confirm that my day trip is “complete” once I like all of the random selections. 
 # (10): Display my completed trip in the console. 
 
-dest_list = ['Beach', 'Zoo', 'Mall', 'Water Park', 'Amusement Park']
+def destinations():
+    dest_list = ['Beach', 'Zoo', 'Mall', 'Water Park', 'Amusement Park']
+    for dest_result in dest_list:
+        print(f'Would you like to visit the {dest_result} today? y/n?')
+        answer = input()
+        if answer == 'n':
+            print('Understood.')
+            print('')
+        elif answer == 'y':
+            print(f'Great News!! Have fun at the {dest_result} today!')
+            print('')
+            break
+destinations()
 
-for dest_result in dest_list:
-    print(f'Would you like to visit the {dest_result} today? y/n?')
-    answer = input()
-    if answer == 'n':
-        print('Understood.')
-    elif answer == 'y':
-        print(f'Great News!! Have fun at the {dest_result} today!')
-        break
-        
+
+def transportations():
+    trans_list = ['Walk', 'Bike', 'Drive', 'Uber', 'Train']
+    for trans_result in trans_list:
+        print(f'How would you like to get there? Is today a/an {trans_result} kind of day? y/n?')
+        answer = input()
+        if answer == 'n':
+            print('Understood.')
+            print('')
+        elif answer == 'y':
+            print(f'Great News!! Have fun taking the {trans_result} today!')
+            print('')
+            break
+transportations()
+
+def diners():
+    dine_list = ['Ceasars Grill', 'Italian Bistro', 'Irish Pub', 'Toro Cafe', 'Sticky Kitchen']
+    for dine_result in dine_list:
+        print(f'After the fun day, Would you like to dine at the {dine_result} for dinner? y/n?')
+        answer = input()
+        if answer == 'n':
+            print('Understood.')
+            print('')
+        elif answer == 'y':
+            print(f'Great News!! Have fun dining at the {dine_result} this evening!')
+            print('')
+            break
+diners()
+
+def shows():
+    show_list = ['Musical Theatre', 'Live Concert', 'Dancing Ball', 'Sporting Event', 'Movie']
+    for show_result in show_list:
+        print(f'After Dinner, would you like to experience a {show_result}? y/n?')
+        answer = input()
+        if answer == 'n':
+            print('Understood.')
+            print('')
+        elif answer == 'y':
+            print(f'Great News!! Have fun watching the {show_result} tonight!')
+            print('')
+            break
+shows()
