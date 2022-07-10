901137c7ecf99f1b25adc8aca224aacc05e7a731
unknown <dylanjlenz@gmail.com>
2022-07-10 15:31:44 -0500

    Ignore alt.py files, final polish main.py file.

--- alt2.py
+++ alt2.py
@@ -1,60 +1,91 @@
-# (15): Randomly re-select a destination, restaurant, transportation, or entertainment. 
-# (10): Confirm that my day trip is “complete” once I like all of the random selections. 
-# (10): Display my completed trip in the console. 
 
-def destinations():
-    dest_list = ['Beach', 'Zoo', 'Mall', 'Water Park', 'Amusement Park']
+dest_list = ['Beach', 'Zoo', 'Mall', 'Water Park', 'Amusement Park']
+trans_list = ['Walk', 'Bike', 'Drive', 'Uber', 'Train']
+dine_list = ['Ceasars Grill', 'Italian Bistro', 'Irish Pub', 'Toro Cafe', 'Sticky Kitchen']
+show_list = ['Musical Theatre', 'Live Concert', 'Dancing Ball', 'Sporting Event', 'Movie']
+
+def greet():
+    print('')
+    print('Welcome to your Personal-Pocket-Planner.') 
+    print('Where everything is handled by us, for you.') 
+    print('')
+    print('Your only responsiblity, choose how you would have fun.')
+    print('We will provide you with plans for the day, dinner, and show.')
+    print('As well as arrange all accomodations necassary, and give you one convenient place to pay.')
+    print('When you are ready, please make your first selection.')
+    print('')
+greet()
+
+def destination():
     for dest_result in dest_list:
         print(f'Would you like to visit the {dest_result} today? y/n?')
         answer = input()
         if answer == 'n':
-            print('Understood.')
-            print('')
+            print('Aplogies.')
         elif answer == 'y':
-            print(f'Great News!! Have fun at the {dest_result} today!')
+            print(f'Great Choice!! Have fun at the {dest_result} today!')
             print('')
             break
-destinations()
 
-
-def transportations():
-    trans_list = ['Walk', 'Bike', 'Drive', 'Uber', 'Train']
+def transportaion():
     for trans_result in trans_list:
         print(f'How would you like to get there? Is today a/an {trans_result} kind of day? y/n?')
         answer = input()
         if answer == 'n':
-            print('Understood.')
-            print('')
+            print('Aplogies.')
         elif answer == 'y':
-            print(f'Great News!! Have fun taking the {trans_result} today!')
+            print(f'Great Choice!! Have fun taking the {trans_result} today!')
             print('')
             break
-transportations()
 
-def diners():
-    dine_list = ['Ceasars Grill', 'Italian Bistro', 'Irish Pub', 'Toro Cafe', 'Sticky Kitchen']
+def dining():
     for dine_result in dine_list:
         print(f'After the fun day, Would you like to dine at the {dine_result} for dinner? y/n?')
         answer = input()
         if answer == 'n':
-            print('Understood.')
-            print('')
+            print('Aplogies.')
         elif answer == 'y':
-            print(f'Great News!! Have fun dining at the {dine_result} this evening!')
+            print(f'Great Choice!! Have fun dining at the {dine_result} this evening!')
             print('')
             break
-diners()
 
-def shows():
-    show_list = ['Musical Theatre', 'Live Concert', 'Dancing Ball', 'Sporting Event', 'Movie']
+def show():
     for show_result in show_list:
         print(f'After Dinner, would you like to experience a {show_result}? y/n?')
         answer = input()
         if answer == 'n':
-            print('Understood.')
-            print('')
+            print('Aplogies.')
         elif answer == 'y':
-            print(f'Great News!! Have fun watching the {show_result} tonight!')
+            print(f'Great Choice!! Have fun watching the {show_result} tonight!')
             print('')
             break
-shows()
+
+result_list = [dest_result, trans_result, dine_result, show_result]
+
+for i in result_list:
+    print(f'You have made some wonderful choices.') 
+    print(f'Your plan is to take the {trans_result} to the {dest_result}.')
+    print(f'After that you will dine at {dine_result}, then watch a {show_result}.')
+    print('')
+    print('If you confirm these plans, please choose y/n.')
+    answer = input()
+    if answer == 'n':
+        print('Aplogies. Let us try again.')
+    elif answer == 'y':
+        print(f'Awesome!! Have fun! Glad we could Help.')
+        print('')
+        break
+
+dest_lst = ['Beach', 'Zoo', 'Mall', 'Water Park', 'Amusement Park']
+
+def dest_sel():
+    for dest_ans in dest_lst:
+        dest_opt = input(f'Would you like to visit the {dest_ans} today? y/n? :')
+        if dest_opt == 'y':
+            print(f'Great Choice!! Have fun at the {dest_ans} today!')
+            return dest_ans
+        else:
+            print('Let us try again...')
+
+dest_ch = dest_sel()
+trip_lst = [dest_ch]
--- main.py
+++ main.py
@@ -1,13 +1,88 @@
-# (5): Store my destinations, restaurants, mode of transportations, and entertainments on separate lists. 
-# (5): Destination to be randomly selected. 
-# (5): Restaurant to be randomly selected. 
-# (5): Transportation to be randomly selected. 
-# (5): Entertainment to be randomly selected. 
 
-# (15): Randomly re-select a destination, restaurant, transportation, or entertainment. 
-# (10): Confirm that my day trip is “complete” once I like all of the random selections. 
-# (10): Display my completed trip in the console. 
+# Suffix Key:
+# _ans = answer
+# _sel = select
+# _opt = option
+# _lst = list
+# _ch  = choice
 
-# (5): All of my functions have a Single Responsibility. Each function should do one thing.
-# (5): Make at least three commits with descriptive messages. 
+dest_lst = ['Beach', 'Zoo', 'Golf', 'Water Park', 'Amusement Park']
+trans_lst = ['Walk', 'Bike', 'Drive', 'Uber', 'Train']
+dine_lst = ['Ceasars Grill', 'Italian Bistro', 'Irish Pub', 'Toro Cafe', 'Sticky Kitchen']
+show_lst = ['Live Musical Theatre', 'Live Concert', 'Wrestling Match', 'Sporting Event', 'Movie']
 
+def greet():
+    print('')
+    print('Welcome to your Personal-Pocket-Planner.') 
+    print('Where everything is handled by us, for you.') 
+    print('')
+    print('Your only responsiblity, choose how you would like to have fun.')
+    print('We will provide you with plans for the day, dinner, and a show.')
+    print('As well as arrange all accomodations necassary while giving you one convenient place to pay.')
+    print('When you are ready, please make your first selection.')
+    print('')
+
+def dest_sel():
+    for dest_ans in dest_lst:
+        dest_opt = input(f'Would you like to visit the {dest_ans} today? y/n? :')
+        if dest_opt == 'y':
+            print(f'Great Choice!! Have fun at the {dest_ans} today!')
+            return dest_ans
+        else:
+            print('Let us try again...')
+
+
+def trans_sel():
+    for trans_ans in trans_lst:
+        trans_opt = input(f'How would you like to get there? Is today a/an {trans_ans} kind of day? y/n?')
+        if trans_opt == 'y':
+            print(f'Great Choice!! Have fun taking the {trans_ans} today!')
+            return trans_ans
+        else:
+            print('Let us try again...')
+
+def dine_sel():
+    for dine_ans in dine_lst:
+        dine_opt = input(f'After the fun day, Would you like to dine at the {dine_ans} for dinner? y/n?')
+        if dine_opt == 'y':
+            print(f'Great Choice!! Have fun dining at the {dine_ans} this evening!')
+            return dine_ans
+        else:
+            print('Let us try again...')
+
+def show_sel():
+    for show_ans in show_lst:
+        show_opt = input(f'After Dinner, would you like to experience a {show_ans}? y/n?')
+        if show_opt == 'y':
+            print(f'Great Choice!! Have fun watching the {show_ans} tonight!')
+            return show_ans
+        else:
+            print('Let us try again...')
+
+def trip_sel():
+    greet()
+    dest_ch = dest_sel()
+    print('')
+    trans_ch = trans_sel()
+    print('')
+    dine_ch = dine_sel()
+    print('')
+    show_ch = show_sel()
+    print('')
+    trip_lst = [dest_ch, trans_ch, dine_ch, show_ch]
+    print('Your Request Has been submitted.')
+    print('')
+    print(f'Your plan for today is to take the {trans_ch} to the {dest_ch}.')
+    print(f'Then this evening, you will dine at The World Famous {dine_ch} and then watch a {show_ch}.')
+    print('')
+    for i in trip_lst:
+        trip_opt = input('Would you like to finalize your plan for today? y/n? :')
+        if trip_opt == 'y':
+            print('Great Choices!! Have fun!')
+            return i
+        else:
+            print('')
+            print('Let us start over...')
+            trip_sel()
+
+trip_sel()
