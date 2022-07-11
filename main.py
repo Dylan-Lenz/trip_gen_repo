ff4641acb14856b8f60fdcf07d9183e2ff36367f
unknown <dylanjlenz@gmail.com>
2022-07-11 11:19:29 -0500

    Modify Main, Fix Negative Bug, Create/Add Start-Over Feature.

--- main.py
+++ main.py
@@ -6,10 +6,11 @@
 # _lst = list
 # _ch  = choice
 
-dest_lst = ['Beach', 'Zoo', 'Golf', 'Water Park', 'Amusement Park']
-trans_lst = ['Walk', 'Bike', 'Drive', 'Uber', 'Train']
-dine_lst = ['Ceasars Grill', 'Italian Bistro', 'Irish Pub', 'Toro Cafe', 'Sticky Kitchen']
-show_lst = ['Live Musical Theatre', 'Live Concert', 'Wrestling Match', 'Sporting Event', 'Movie']
+
+dest_lst = ['Beach', 'Zoo', 'Golf Course', 'Water Park', 'Amusement Park', 'Start Over,']
+trans_lst = ['Walk', 'Bike', 'Drive', 'Uber', 'Train', 'Start Over,']
+dine_lst = ['Ceasars Grill', 'Italian Bistro', 'Irish Pub', 'Toro Cafe', 'Sticky Kitchen', 'Start Over,']
+show_lst = ['Live Musical Theatre', 'Live Concert', 'Wrestling Match', 'Sporting Event', 'Movie', 'Start Over,']
 
 def greet():
     print('')
@@ -23,19 +24,24 @@ def greet():
     print('')
 
 def dest_sel():
-    for dest_ans in dest_lst:
+   for dest_ans in dest_lst:
         dest_opt = input(f'Would you like to visit the {dest_ans} today? y/n? :')
-        if dest_opt == 'y':
-            print(f'Great Choice!! Have fun at the {dest_ans} today!')
+        if dest_ans == 'Start Over,' and dest_opt == 'y':
+            print('')
+            dest_sel()
+        elif dest_opt == 'y':
+            print(f'Great Choice!! Have fun visiting the {dest_ans} today!')
             return dest_ans
         else:
             print('Let us try again...')
 
-
 def trans_sel():
     for trans_ans in trans_lst:
-        trans_opt = input(f'How would you like to get there? Is today a/an {trans_ans} kind of day? y/n?')
-        if trans_opt == 'y':
+        trans_opt = input(f'How would you like to get there? Is today a/an {trans_ans} kind of day? y/n? :')
+        if trans_ans == 'Start Over,' and trans_opt == 'y':
+            print('')
+            trans_sel()
+        elif trans_opt == 'y':
             print(f'Great Choice!! Have fun taking the {trans_ans} today!')
             return trans_ans
         else:
@@ -43,8 +49,11 @@ def trans_sel():
 
 def dine_sel():
     for dine_ans in dine_lst:
-        dine_opt = input(f'After the fun day, Would you like to dine at the {dine_ans} for dinner? y/n?')
-        if dine_opt == 'y':
+        dine_opt = input(f'After the fun day, Would you like to dine at the {dine_ans} for dinner? y/n? :')
+        if dine_ans == 'Start Over,' and dine_opt == 'y':
+            print('')
+            dine_sel()
+        elif dine_opt == 'y':
             print(f'Great Choice!! Have fun dining at the {dine_ans} this evening!')
             return dine_ans
         else:
@@ -52,8 +61,11 @@ def dine_sel():
 
 def show_sel():
     for show_ans in show_lst:
-        show_opt = input(f'After Dinner, would you like to experience a {show_ans}? y/n?')
-        if show_opt == 'y':
+        show_opt = input(f'After Dinner, would you like to experience a {show_ans}? y/n? :')
+        if show_ans == 'Start Over,' and show_opt == 'y':
+            print('')
+            show_sel()
+        elif show_opt == 'y':
             print(f'Great Choice!! Have fun watching the {show_ans} tonight!')
             return show_ans
         else:
